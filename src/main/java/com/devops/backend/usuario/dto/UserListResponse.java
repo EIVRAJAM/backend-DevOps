7566a5a86139b6794ecf95ec72648b286a297755
Juan Sebastian Martinez <170745116+JsMartinez-dev@users.noreply.github.com>
2026-05-01 18:42:30 -0500

    fix(UserListResponse) Change the @Shema for estado and genero

--- src/main/java/com/devops/backend/usuario/dto/UserListResponse.java
+++ src/main/java/com/devops/backend/usuario/dto/UserListResponse.java
@@ -11,12 +11,12 @@ public record UserListResponse(
         @Schema(description = "Apellidos del usuario", example = "Pérez García") String apellidos,
 
         @Schema(description = "Número de documento de identificación", example = "1234567890") String documento,
-        @Schema(description = "Número de documento de identificación", example = "1234567890") String genero,
+        @Schema(description = "Genero del usuario", example = "1234567890") String genero,
 
         @Schema(description = "Fecha de nacimiento del usuario", example = "1990-05-15") String fechaNacimiento,
 
         @Schema(description = "Número de teléfono del usuario", example = "+57 3001234567") String telefono,
-        @Schema(description = "Número de teléfono del usuario", example = "+57 3001234567") String estado
+        @Schema(description = "Estado del usuario", example = "+57 3001234567") String estado
 
         ) {
 }
