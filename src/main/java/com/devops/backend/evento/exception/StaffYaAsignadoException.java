4b555b57679f463c08b51dd3d9c032600a374565
B4KI <125510360+SeumaRayo@users.noreply.github.com>
2026-05-18 01:50:30 -0500

    feat(exception): add custom exceptions for check-in process validation

---
+++ src/main/java/com/devops/backend/evento/exception/StaffYaAsignadoException.java
@@ -0,0 +1,7 @@
+package com.devops.backend.evento.exception;
+
+public class StaffYaAsignadoException extends RuntimeException {
+    public StaffYaAsignadoException(String message) {
+        super(message);
+    }
+}
