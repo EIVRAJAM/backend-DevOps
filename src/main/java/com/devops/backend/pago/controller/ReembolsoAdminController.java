e693dd9f87d93668bbb431969c5aed5c9b4071cf
B4KI <125510360+SeumaRayo@users.noreply.github.com>
2026-05-20 11:19:51 -0500

    feat(controller): add PagoController, ReembolsoAdminController, ReembolsoOrganizadorController, and ReembolsoUsuarioController for managing payments and reimbursement requests

---
+++ src/main/java/com/devops/backend/pago/controller/ReembolsoAdminController.java
@@ -0,0 +1,9 @@
+package com.devops.backend.pago.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reembolsos")
+public class ReembolsoAdminController {
+}
